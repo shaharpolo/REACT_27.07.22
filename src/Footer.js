efb9d3411b6adead26a312042cfbae26f3d5e954
shahar <shaharpolo@gmail.com>
2022-08-26 15:50:19 +0300

    first commit

---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div><div class="mt-5 p-4 bg-dark text-white text-center">
+    <p>Footer</p>
+  </div></div>
+  )
+}
+
+export default Footer
---
+++ src/MyData.js
@@ -0,0 +1,6 @@
+export const products=[
+    {desc: "milk", price: 5},
+    {desc: "toy", price: 15},
+    {desc: "koteg", price: 4},
+
+]
